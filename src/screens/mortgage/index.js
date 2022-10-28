1735af2bec7e22f2bb47ba45c094cef5c0a6918a
jawad halby makary <jawadhalbymakary@gmail.com>
2022-10-28 21:57:35 +0300

    added mortgage calculator

--- src/screens/mortgage/index.js
+++ src/screens/mortgage/index.js
@@ -1,12 +1,35 @@
 import React from "react";
 import Banner from "../../components/Banner";
 import Header from "../../components/Header";
-
+import MortgageCalculator from "mortgage-calculator-react";
+import "./index.css";
+import Footer from "../../components/Footer";
 const Mortgage = () => {
   return (
     <div className="mortgage">
       <Header />
       <Banner />
+      <div className="mortgage__container">
+        <div className="mortgage__left">
+          <h2 className="mortgage__header">Mortgage Calculator</h2>
+          <span className="mortgage__text">
+           Lorem ipsum dolor sit amet
+          </span>
+        </div>
+        <div className="mortgage__right">
+          <MortgageCalculator
+            price={125000}
+            downPayment={10000}
+            interestRate={0.065}
+            months={120}
+            additionalPrincipalPayment={100}
+            taxRate={0.01}
+            insuranceRate={0.01}
+            mortgageInsuranceEnabled={false}
+          />
+        </div>
+      </div>
+      <Footer/>
     </div>
   );
 };
